cf4d569650991e01b4cf68930a3e1398b978f5e3
Eric Hofesmann <ehofesmann@gmail.com>
2017-04-19 20:16:19 -0400

    Begin Adding Functionality to Alarms

--- teamMalagaAlarmClock/src/com/csci360/alarmclock/FXMLController.java
+++ teamMalagaAlarmClock/src/com/csci360/alarmclock/FXMLController.java
@@ -98,14 +98,160 @@ public class FXMLController implements UITimingInterface {
     @FXML
     private Button volUp;
     
+    @FXML
+    private Button incHourA1;
+    @FXML
+    private Button decHourA1;
+    @FXML
+    private Button incHourA2;
+    @FXML
+    private Button decHourA2;
+    @FXML
+    private Button incMinA1;
+    @FXML
+    private Button decMinA1;
+    @FXML
+    private Button incMinA2;
+    @FXML
+    private Button decMinA2;
+    @FXML
+    private Button incMinA1Fast;
+    @FXML
+    private Button decMinA1Fast;
+    @FXML
+    private Button incMinA2Fast;
+    @FXML
+    private Button decMinA2Fast;
+    
+    @FXML
+    private Label hourA1;
+    @FXML
+    private Label minA1;
+    @FXML
+    private Label hourA2;
+    @FXML
+    private Label minA2;
+    
+    @FXML
+    private RadioButton am1;
+    @FXML
+    private RadioButton pm1;
+    @FXML
+    private RadioButton am2;
+    @FXML
+    private RadioButton pm2;
+    
     private Controller controller;
 
+    
+    int alarm1Hour;
+    int alarm2Hour;
+    boolean alarm1isAM;
+    int alarm1Min;
+    int alarm2Min;
+    boolean alarm2isAM;
+    
     public FXMLController(){
        controller = new Controller(this);
-
+       this.controller.setHourA1(12);
+       this.controller.setMinuteA1(0);
+       this.controller.setHourA2(12);
+       this.controller.setMinuteA2(0);
+       this.alarm1isAM = false;
+       this.alarm2isAM = false;
     }
     
+    
+    @FXML
+    public void incrementHourA1(ActionEvent event){
+    this.controller.incrementHourA1();
+        this.hourA1.setText(String.format("%02d", this.controller.getHourA1()));
+    }
+    
+    @FXML
+    public void decrementHourA1(ActionEvent event){
+        this.controller.decrementHourA1();
+        this.hourA1.setText(String.format("%02d", this.controller.getHourA1()));
+    }
+    
+    @FXML
+    public void incrementHourA2(ActionEvent event){
+        this.controller.incrementHourA2();
+        this.hourA2.setText(String.format("%02d", this.controller.getHourA2()));
+    }
+    
+    @FXML
+    public void decrementHourA2(ActionEvent event){
+        this.controller.decrementHourA2();
+        this.hourA2.setText(String.format("%02d", this.controller.getHourA2()));
+    }
+    @FXML
+    public void incrementMinA1(ActionEvent event){
+        this.controller.incrementMinA1();
+        this.minA1.setText(String.format("%02d", this.controller.getMinuteA1()));
+    }
+    
+    @FXML
+    public void decrementMinA1(ActionEvent event){
+        this.controller.decrementMinA1();
+        this.minA1.setText(String.format("%02d", this.controller.getMinuteA1()));
+    }
+    
+    @FXML
+    public void incrementMinA2(ActionEvent event){
+        this.controller.incrementMinA2();
+        this.minA2.setText(String.format("%02d", this.controller.getMinuteA2()));
+    }
+    
+    @FXML
+    public void decrementMinA2(ActionEvent event){
+        this.controller.decrementMinA2();
+        this.minA2.setText(String.format("%02d", this.controller.getMinuteA2()));
+    }
+    @FXML
+    public void incrementMinA1Fast(ActionEvent event){
+        for(int i = 0; i < 10; i++){
+            this.controller.incrementMinA1();
+        }
+        this.minA1.setText(String.format("%02d", this.controller.getMinuteA1()));
+    }
+    
+    @FXML
+    public void decrementMinA1Fast(ActionEvent event){
+        for(int i = 0; i < 10; i++){
+            this.controller.decrementMinA1();
+        }
+        this.minA1.setText(String.format("%02d", this.controller.getMinuteA1()));
+    }
+    
+    @FXML
+    public void incrementMinA2Fast(ActionEvent event){
+        for(int i = 0; i < 10; i++){
+            this.controller.incrementMinA2();
+        }
+        this.minA2.setText(String.format("%02d", this.controller.getMinuteA2()));
+    }
+    
+    @FXML
+    public void decrementMinA2Fast(ActionEvent event){
+        for(int i = 0; i < 10; i++){
+            this.controller.decrementMinA2();
+        }
+        this.minA2.setText(String.format("%02d", this.controller.getMinuteA2()));
+    }
+    
+    @FXML
+    public void setAlarm1(ActionEvent event){
+        this.controller.setAlarm1();
+        this.alarm1text.setText(String.format("%02d", this.alarm1Hour) + ":"+String.format("%02d", this.alarm1Min));
+    }
 
+    @FXML
+    public void setAlarm2(ActionEvent event){
+        this.controller.setAlarm2();
+        this.alarm2text.setText(String.format("%02d", this.alarm2Hour) + ":"+String.format("%02d", this.alarm2Min));
+    }
+    
     
     @FXML
     public void freqUp(ActionEvent event){
@@ -154,11 +300,35 @@ public class FXMLController implements UITimingInterface {
     @FXML
     public void toggleAMPM(ActionEvent event){
         this.controller.setMilitaryTime(false);
+        this.controller.setHourA1(12);
+        this.controller.setMinuteA1(0);
+        this.controller.setHourA2(12);
+        this.controller.setMinuteA2(0);
+        this.hourA1.setText(String.format("%02d", this.controller.getHourA1()));
+        this.minA1.setText(String.format("%02d", this.controller.getMinuteA1()));
+        this.hourA2.setText(String.format("%02d", this.controller.getHourA2()));
+        this.minA2.setText(String.format("%02d", this.controller.getMinuteA2()));
+        this.am1.setDisable(false);
+        this.pm1.setDisable(false);
+        this.am2.setDisable(false);
+        this.pm2.setDisable(false);
     }
     
     @FXML
     public void toggle24Hour(ActionEvent event){
+        this.controller.setHourA1(12);
+        this.controller.setMinuteA1(0);
+        this.controller.setHourA2(12);
+        this.controller.setMinuteA2(0);
         this.controller.setMilitaryTime(true);
+        this.hourA1.setText(String.format("%02d", this.controller.getHourA1()));
+        this.minA1.setText(String.format("%02d", this.controller.getMinuteA1()));
+        this.hourA2.setText(String.format("%02d", this.controller.getHourA2()));
+        this.minA2.setText(String.format("%02d", this.controller.getMinuteA2()));
+        this.am1.setDisable(true);
+        this.pm1.setDisable(true);
+        this.am2.setDisable(true);
+        this.pm2.setDisable(true);
     }
 
     public void updateMilitaryTime(int hours, int minutes) {
